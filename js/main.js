9ff1a71889393a640a4ad207e270746740368f7f
phplpsummer <sh052100@naver.com>
2021-11-22 18:03:37 +0900

    bg 변경기능 추가

--- js/main.js
+++ js/main.js
@@ -22,3 +22,31 @@ const swiper = new Swiper('#wrap',{
     
 
 });
+
+
+const bgs = document.querySelectorAll(".bg li");
+const prev = document.querySelector(".swiper-button-prev");
+const next = document.querySelector(".swiper-button-next");
+const navi = document.querySelectorAll(".swiper-pagination span");
+
+// prev.addEventListener("click",activation);
+// next.addEventListener("click",activation);
+// window.addEventListener("mousewheel",activation);
+
+for(let el of navi){
+    el.addEventListener("click",(e)=>{
+        var isOn = e.currentTarget.classList.contains("swiper-pagination-bullet-active");
+        if(isOn) return;
+        swiper.on("slideChangeTransitionEnd",activation);
+    });
+}
+
+function activation(){
+    let item = document.querySelector(".swiper-slide-active");
+    let i = item.getAttribute("data-swiper-slide-index");
+
+    for(let el of bgs){
+        el.classList.remove("on");
+    }
+    bgs[i].classList.add("on");
+}
